6ac817eb9e1a921a93fddb5550168d0f6184b9e8
Jeremy Swinnen <jswinnen@me.com>
2018-07-16 23:55:16 +0200

    Adding masthead-modal script
    - Removing unused files in /assets/scripts/
    - Tweaking '.c-masthead-modal .c-nav' styling

--- _src/scripts/site.js
+++ _src/scripts/site.js
@@ -2,8 +2,18 @@
 // Imports
 // =======================================================================
 
-// @codekit-append  "wow.min.js"
+// Imported scripts go here ;)
 
 // =======================================================================
 // Site scripts
 // =======================================================================
+
+var mastheadToggle = document.querySelector('.c-masthead__toggle');
+var mastheadModal = document.querySelector('.c-masthead-modal');
+
+var handleToggleClick = function handleToggleClick(e) {
+   e.preventDefault();
+   mastheadModal.classList.toggle('c-masthead-modal--open');
+};
+
+mastheadToggle.addEventListener('click', handleToggleClick, false);
