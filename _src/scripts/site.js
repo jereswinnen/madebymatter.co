914aa62da0b254ed43c1588d57ef5c4fa8d11d95
Jeremy Swinnen <jswinnen@me.com>
2018-07-17 01:56:50 +0200

    Masthead refactor
    - Made 'c-masthead-toggle part of the body instead of being a child of 'c-masthead'
    - Adjusted css
    - Updated 'site.js' to reflect markup and css changes
    - Animating 'c-masthead-toggle' (needs tweaking)
    - Fixed spacing between nav items on larger breakpoints

--- _src/scripts/site.js
+++ _src/scripts/site.js
@@ -8,12 +8,13 @@
 // Site scripts
 // =======================================================================
 
-var mastheadToggle = document.querySelector('.c-masthead__toggle');
+var mastheadToggle = document.querySelector('.c-masthead-toggle');
 var mastheadModal = document.querySelector('.c-masthead-modal');
 
-var handleToggleClick = function handleToggleClick(e) {
+var ToggleClick = function ToggleClick(e) {
    e.preventDefault();
    mastheadModal.classList.toggle('c-masthead-modal--open');
+   mastheadToggle.classList.toggle('c-masthead-toggle--click');
 };
 
-mastheadToggle.addEventListener('click', handleToggleClick, false);
+mastheadToggle.addEventListener('click', ToggleClick, false);
